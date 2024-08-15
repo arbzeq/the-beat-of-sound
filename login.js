5e2629ad3cab0b8d076d36b813a1cc96239e49f1
Arbnor Zeqiri <arbnor.zeqiri@yahoo.com>
2024-08-15 11:18:15 +0200

    .

--- login.js
+++ login.js
@@ -1,3 +1,65 @@
 window.addEventListener("load", () => {
-    console.log("Yo");
+    console.log("login.js");
+    let form = document.querySelector("form");
+    let info = document.querySelector("#info");
+
+
+    form.addEventListener("submit", (event) => {
+        
+        info.style.display = "none";
+        info.style.color = "green";
+
+        event.preventDefault();
+
+        try {
+            
+            let username = event.target.username.value;
+            let password = event.target.password.value;
+            console.log("password is ", password);
+            
+            if(username.length < 4){
+                throw new Error("Invalid username.");
+            }
+            //Get accounts from localstorage. Returns null if it doesn't exist.
+            let accounts = JSON.parse(localStorage.getItem("accounts"));
+
+            //If the accounts object in localStorage is empty, create an empty one.
+            if(!accounts){
+                accounts = [];
+            }
+            
+            let userAccount = null;
+            for(let account of accounts) {
+                console.log(account);
+                if(username == account["username"]){
+                    userAccount = account;
+                }
+            };
+
+            if(userAccount){
+                console.log("Useraccount", userAccount);
+                if(password == userAccount["password"]){
+                    localStorage.setItem("activeAccount", JSON.stringify(userAccount));
+                    location.href = ""
+                    
+                }else{
+                    throw new Error("Wrong password.")
+                }
+            }else{
+                throw new Error("Username does not exist.")
+            }
+
+           
+            //throw new Error("Something went wrong, try again.")
+        }catch(errorMessage){
+            console.log(errorMessage);
+            info.textContent = errorMessage;
+            info.style.color = "red";
+        }
+
+        info.style.display = "block";
+        
+    })
+    
 });
+
---
+++ register.js
@@ -0,0 +1,69 @@
+window.addEventListener("load", () => {
+    console.log("register.js");
+    let form = document.querySelector("form");
+    
+    console.log(form);
+    let info = document.querySelector("#info");
+
+    form.addEventListener("submit", (event) => {
+        //Show info
+        info.style.display = "block";
+
+        event.preventDefault();
+
+        console.log(event);
+        try {
+            
+            let username = event.target.username.value;
+            let password = event.target.password.value;
+            let confirmPassword = event.target.confirmPassword.value;
+            
+            if(username.length < 4){
+                throw new Error("Username must be at least 4 characters!");
+            }
+            //Get accounts from localstorage. Returns null if it doesn't exist.
+            let accounts = JSON.parse(localStorage.getItem("accounts"));
+
+            
+            if(password != confirmPassword) {
+                throw new Error("Passwords do not match!");
+            }
+
+
+            let newAccount = {
+                "username": username,
+                "password": password
+            };
+
+            
+            /*Add the account to accounts if username not taken.
+            Create new array if accounts is empty.*/
+            if(accounts) {
+                let allUsernames = [];
+        
+                accounts.forEach((account) => {
+                    allUsernames.push(account["username"]);
+                });
+        
+                if(allUsernames.includes(username)){
+                    throw new Error("Username already exists!");
+                }
+
+                accounts.push(newAccount);
+            }else{
+                accounts = [newAccount];
+            }
+            
+            localStorage.setItem("accounts", JSON.stringify(accounts));
+            
+            info.textContent = "Account successfully created.";
+            info.style.color = "green";
+        }catch(errorMessage){
+            info.textContent = errorMessage;
+            info.style.color = "red";
+        }
+        
+    })
+    
+});
+
